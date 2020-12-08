b0366cc04e299270a64f52740b87f3dff1d441ed
Yurii Fedelesh <yurii.fedelesh@gmail.com>
2020-12-08 17:28:41 +0200

    Init

---
+++ Chat-Back/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Chat_Back.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Back
+{
+    public class ChatHub : Hub
+    {
+        public Task SendAsync(Message message)
+        {
+            return Clients.All.SendAsync("SendAsync", message);
+        }
+    }
+}
---
+++ Chat-Back/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace Chat_Back.Models
+{
+    public class Message
+    {
+        public string Username { get; set; }
+        public string Text { get; set; }
+    }
+}
